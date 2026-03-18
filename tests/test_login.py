3eb25518faef68747ea4962cacf1a6c4cb457da6
Vinicius Frois <viniifrois@gmail.com>
2026-03-18 14:44:41 -0300

    Correção pipeline e imports desnecessários

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,10 +2,7 @@ import os
 from pages.login import LoginPage
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def criar_driver():
